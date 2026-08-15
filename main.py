793e1dc42406842f4e791142829ede15fbded75b
binaryCoder-101 <syed.misak@gmail.com>
2026-08-15 20:25:56 +0530

    OpenAI and Anthropic models now functional

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ print(f"langgraph version: {lg_version}")
 def main():
     print("Hello from paulo-dicole-langchain-projects!")
 
-    # llm_openai = ChatOpenAI(model="gpt-4o-mini", temperature=0)
-    # response_openai = llm_openai.invoke("Say 'setup complete' in one word")
-    # print(f"Response from ChatOpenAI: {response_openai}")
+    llm_openai = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+    response_openai = llm_openai.invoke("Say 'setup complete' in one word")
+    print(f"Response from ChatOpenAI: {response_openai}")
 
-    # llm_anthropic = ChatAnthropic(model="claude-sonnet-4-5-20250929", temperature=0)
-    # response_anthropic = llm_anthropic.invoke("Say 'setup complete' in one word")
-    # print(f"Response from ChatAnthropic: {response_anthropic}")
+    llm_anthropic = ChatAnthropic(model="claude-sonnet-4-5-20250929", temperature=0)
+    response_anthropic = llm_anthropic.invoke("Say 'setup complete' in one word")
+    print(f"Response from ChatAnthropic: {response_anthropic}")
 
     llm_openrouter = ChatOpenRouter(model="openrouter/free", temperature=0)
     response_openrouter = llm_openrouter.invoke("Say 'setup complete' in one word")
