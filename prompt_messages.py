546b6f058d772db8fdc1a84ff4dc25054569cd11
binaryCoder-101 <syed.misak@gmail.com>
2026-08-17 19:40:00 +0530

    multi-message template

--- prompt_messages.py
+++ prompt_messages.py
@@ -8,8 +8,31 @@ from langchain_core.messages import HumanMessage, SystemMessage
 
 load_dotenv()
 
+# ChatPromptTemplate
 prompt = ChatPromptTemplate.from_template("Tell me a {adjective} joke about {topic}.")
 
 messages = prompt.format_messages(adjective="funny", topic="chickens")
+
+print(messages)
+
+
+# Multi-message template
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        (
+            "system",
+            "You are a helpful assistant that translates {input_language} to {output_language}.",
+        ),
+        (
+            "human",
+            "Translate the following text: {text}",
+        ),
+    ]
+)
+
+messages = prompt.format_messages(
+    input_language="English", output_language="French", text="I love programming (for the money ofcourse!)"
+)
 
 print(messages)
